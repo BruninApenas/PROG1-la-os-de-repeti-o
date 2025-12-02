e70e7fc661b3c63276221f93da51e1aa6169cd74
BruninApenas <amaralfarias504@gmail.com>
2025-12-02 10:08:42 -0300

    Update lista4.cpp

--- lista4.cpp
+++ lista4.cpp
@@ -41,4 +41,20 @@ cout<<"Soma "<<soma;
 return 0;
 }
 
+#include <iostream> //Questão 1
+using namespace std; //Faça um programa que lê 5 valores inteiros e mostre a soma deles no final da execução
+int main() {
+    int n=1;
+    int valor;
+    int soma;
+    while (n<=5)
+    {
+    cout <<"Digite um valor ";
+    cin>>valor;
+    soma = soma + valor;
+    n++;
+    }
+cout<<"Soma "<<soma;
+return 0;
+}
 
